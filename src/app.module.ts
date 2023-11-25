80395ada30f1a19bc943cce85eddef54b4118acd
Chun-Kai Wang <chunkai1312@gmail.com>
2023-11-25 17:36:03 +0800

    feat: setup app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { FugleMarketDataModule } from '@fugle/marketdata-nest';
+import { LineNotifyModule } from 'nest-line-notify';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    FugleMarketDataModule.forRoot({
+      apiKey: process.env.FUGLE_MARKETDATA_API_KEY,
+    }),
+    LineNotifyModule.forRoot({
+      accessToken: process.env.LINE_NOTIFY_ACCESS_TOKEN,
+    }),
+  ],
+})
 export class AppModule {}
